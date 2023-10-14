ac8540375d089a59ff69c9a9ddeb666dd5e31cde
Mariana <mariana.urcuqui01@gmail.com>
2023-10-14 13:57:28 -0500

    now the user can copy the hex value just with a click

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 
 const pickColorForm = document.getElementById("pick-color-form")
+const colorsDiv = document.getElementById("colors")
+
 
 renderColors('https://www.thecolorapi.com/scheme?hex=24B1E0&mode=monochrome')
 
@@ -8,28 +10,40 @@ pickColorForm.addEventListener("submit", e =>{
   const color = document.getElementById("color")
   const mode = document.getElementById("mode")
 
-  //Here we are taking what the user selected and with that we define the right path in the API
+  //Here we are taking what the user selected 
+  //and with that we define the right path in the API
   const colorHex = color.value
   const path = `
     https://www.thecolorapi.com/scheme?hex=${colorHex.substring(1)}&mode=${mode.value}
   `
-
   renderColors(path)
 })
 
+//This event listener allows the user to copy the hex value 
+//when they click either the image or the hex value
+colorsDiv.addEventListener("click", e => {
+  if(e.target.dataset.hex){
+    const hexValue = e.target.dataset.hex
+    navigator.clipboard.writeText(hexValue);
+  }
+})
+
+//------------------------
+//----- functions --------
+//------------------------ 
 
 function renderColors(path){
   fetch(path)
   .then(res => res.json())
   .then(scheme => {
-    const colorsDiv = document.getElementById("colors")
     const colorsArray = scheme.colors
     let html = ""
     for(let color of colorsArray){
+      const colorHex = color.hex.value
       html += `
       <div class="color-div">
-        <img src="${color.image.bare}" alt="${color.name.value}" class="color-img">
-        <p>${color.hex.value}</p>
+        <img src="${color.image.bare}" alt="${color.name.value}" class="color-img" data-hex="${colorHex}">
+        <p data-hex="${colorHex}">${colorHex}</p>
       </div>
       `
     } 
